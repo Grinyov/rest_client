55a8001c782734dbe76cf91c41151125a0503914
green <vgrinyov@gmail.com>
2017-03-25 22:36:46 +0200

    add healthcheck controller

---
+++ src/main/java/com/grinyov/spring/rest_client/web/HealthCheckController.java
@@ -0,0 +1,20 @@
+package com.grinyov.spring.rest_client.web;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by vgrinyov.
+ */
+@RestController
+public class HealthCheckController {
+
+    @Autowired
+    private HealthService healthService;
+
+    @RequestMapping("/check")
+    public String checkStatus(){
+        return healthService.getHealth();
+    }
+}
